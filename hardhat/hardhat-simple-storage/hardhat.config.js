32d3a75a5d3e2f1064c465034e18d22ecb282c9c
germansanz93 <germansanz93@gmail.com>
2023-05-07 05:42:15 -0300

    added sepolia network

--- hardhat/hardhat-simple-storage/hardhat.config.js
+++ hardhat/hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.8",
-};
+	defaultNetwork: "hardhat", //Hardhat viene con una red similar a ganache y si no especifico una red al deployar un contrato, lo hara en la red por defecto que es la de hardhat tambien tomara por defecto clave privada y todo lo necesario para hacer el dpeloy
+	networks: {
+		sepolia: {
+			url: SEPOLIA_RPC_URL,
+			accounts: [SEPOLIA_PRIVATE_KEY],
+      chainId: 11155111
+		},
+	},
+	solidity: "0.8.8",
+}
