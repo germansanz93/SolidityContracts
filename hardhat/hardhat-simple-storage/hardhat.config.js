6dc522d12f12cdc3e8348f92669d47cb47da3c63
germansanz93 <germansanz93@gmail.com>
2023-05-07 08:50:18 -0300

    custom hardhat tasks

--- hardhat/hardhat-simple-storage/hardhat.config.js
+++ hardhat/hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,17 @@
+const { task } = require("hardhat/config")
+
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+	const accounts = await hre.ethers.getSigners()
+
+	for (const account of accounts) {
+		console.log(await account.getAddress())
+	}
+})
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
@@ -13,11 +24,11 @@ module.exports = {
 		sepolia: {
 			url: SEPOLIA_RPC_URL,
 			accounts: [SEPOLIA_PRIVATE_KEY],
-      chainId: 11155111
+			chainId: 11155111,
 		},
 	},
 	solidity: "0.8.8",
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY
-  }
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
+	},
 }
